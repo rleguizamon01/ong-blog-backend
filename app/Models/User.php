9e65fd7bdc544ee408eb57884709b6b16ffebb74
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-02 16:35:42 -0300

    Fillable attribute changed

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'role',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -29,7 +31,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
