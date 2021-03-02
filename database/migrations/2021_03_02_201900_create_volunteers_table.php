a5ee007f52bdcba3463a8b8d645a7a1e987388ec
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-02 18:41:59 -0300

    Fields added

--- database/migrations/2021_03_02_201900_create_volunteers_table.php
+++ database/migrations/2021_03_02_201900_create_volunteers_table.php
@@ -15,7 +15,14 @@ class CreateVolunteersTable extends Migration
     {
         Schema::create('volunteers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->date('birthdate');
+            $table->string('body');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
