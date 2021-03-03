c005b63e7247adb76884bf8b740333d2f02e6614
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-02 22:03:22 -0300

    Added Comment model and relationship

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['id','post_id','email','ip','body'];
+
+    public function comments(){
+        return $this->belongsTo(Post::class);
+    }
+}
