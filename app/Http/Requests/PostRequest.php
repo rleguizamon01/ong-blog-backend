a7faf5239d625aa66801cec19fe270fcf31c551a
nicobus <bustamante.n.f@gmail.com>
2021-03-04 17:41:29 -0300

    create post request validator

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'integer',
+            'user_id' => 'integer',
+            'category_id' => 'required',
+            'title' => "required|string|min:1|max:100",
+            'body' => "required|string|min:1",
+            "photo" => "image",
+            'category_id' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'Debe completar el campo :attribute',
+            'category_id.required' => 'Debe seleccionar una categoria',
+            'min' => 'El campo :attribute debe tener al menos :min caracteres',
+            'max' => 'El campo :attribute no bebe superar :max caracteres',
+            'image' => 'La imagen debe ser de formato .jpg, .png, etc'
+        ];
+    }
+}
