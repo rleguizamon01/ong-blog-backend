bdb5b65260d917521c10d1db6d65ec922f8fec75
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-05 17:23:06 -0300

    VolunteerFactory created

---
+++ database/factories/VolunteerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Volunteer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VolunteerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Volunteer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->name,
+            'last_name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone_number' => $this->faker->strval(randomNumber(10, true)),
+            'birthdate' => $this->faker->dateTime,
+            'body' => $this->faker->text,
+        ];
+    }
+}
