0111c5b56c2659b325d4f78d74263569962701d1
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-05 14:30:39 -0300

    Fixed PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Category;
 
 class PostFactory extends Factory
 {
@@ -22,7 +24,12 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->text(),
+            'photo' => $this->faker->imageUrl(),
+            'deleted_at' => $this->faker->randomElement([now(), null]),
         ];
     }
 }
