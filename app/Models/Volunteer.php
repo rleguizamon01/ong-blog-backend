c6c537f239aceb0adf1523a05937594427a1951c
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-05 18:46:19 -0300

    Bug of another ticket fixed

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Volunteer extends Model
 {
@@ -15,6 +16,6 @@ class Volunteer extends Model
         'email',
         'phone_number',
         'birthdate',
-        'body',
+        'body'
     ];
 }
