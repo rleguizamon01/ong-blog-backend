907379226db241d3122ed7f02410b1521a5c07be
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-09 19:50:17 -0300

    Added CommentController.Added methods 'index' and 'show'

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function index(){
+        return $comment = Comment::latest()->paginate();
+    }
+    public function show(Comment $comment)
+    {
+        return $comment;
+
+    }
+}
