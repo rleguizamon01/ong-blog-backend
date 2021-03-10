3772205ff3a2758e497a5cbdac60e02215ad9a06
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-10 20:06:05 -0300

    Changed route for resource

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/posts/{post}/comments', [\App\Http\Controllers\CommentController::class, 'index'])->name('comment.index');
-Route::get('/posts/{post}/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'show'])->name('comment.show');
+Route::resource('comments', CommentController::class);
