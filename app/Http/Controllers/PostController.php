d1b2cff44eb1b2fe59eceea9542511cdf5d9cb17
Paz Marzana <pazmarzana@gmail.com>
2021-03-10 17:15:39 -0300

    Updated PostController, added update function (the others had been already commited)

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Category;
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate(10);
+        return $posts;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::select('id','name')->orderBy('name','ASC')->get();
+        return view('posts.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        $data = array_diff_key($request->all(), array_flip(['photo']));
+        $imageAux = $request->photo;
+        if ($imageAux){
+            $imageLongPath = time() . $imageAux->getClientOriginalName();
+            \Storage::disk('images')->put($imageLongPath, \File::get($imageAux));
+            $data['photo'] = $imageLongPath;
+        }
+        $data['user_id'] = Auth::id();
+        $post = Post::create($data);
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        $post = Post::With('comments')->find($post->id);
+        return $post;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::select('id','name')->orderBy('name','ASC')->get();
+        return view('posts.edit',compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        $data = array_diff_key($request->all(), array_flip(['photo']));
+        $imageAux = $request->photo;
+        if ($imageAux){
+            $imageLongPath = time() . $imageAux->getClientOriginalName();
+            \Storage::disk('images')->put($imageLongPath, \File::get($imageAux));
+            $data['photo'] = $imageLongPath;
+        }
+        $data['user_id'] = Auth::id();
+        $post -> update($data);
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
