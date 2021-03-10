bbf97d965954d3a49a9a0cb81c4309ae3ff49b12
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-10 19:58:30 -0300

    Added edit view

---
+++ resources/views/comments/edit.blade.php
@@ -0,0 +1,31 @@
+
+
+<form method="POST" action="{{route('comments.update',$comment->id)}}">
+    @csrf
+    @method('PATCH')
+
+    <label for="email">Email: </label>
+    <input type="text" name="email"
+
+           @error('email') class="danger" @enderror
+           value="{{ $comment->email }}">
+
+    @error('email')
+    <p class="error">{{ $errors->first('email') }}</p>
+    @enderror
+
+    <br>
+
+    <label for="body">Body:</label>
+    <textarea @error('body') class="danger" @enderror name="body" id="body" cols="30" rows="10">
+         {{ $comment->body }}
+     </textarea>
+
+    @error('body')
+    <p class="error">{{ $errors->first('body') }}</p>
+    @enderror
+
+    <br>
+
+    <button class="button special fit" type="submit">Send</button>
+</form>
