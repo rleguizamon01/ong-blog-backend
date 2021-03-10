90b118e08bad50009912cf5aaa15bc41407c4071
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-10 15:15:30 -0300

    Fixed methods index and show. Fixed routes

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function index(){
-        return Comment::latest()->paginate();
+    public function index(Post $post){
+
+        return Comment::where('post_id','=',$post->id)->paginate();
+
     }
-    public function show(Comment $comment)
-    {
+    public function show(Post $post,Comment $comment)
+    {   if ($post->id = $comment->post_id){
         return $comment;
+
+    }else{
+        abort(404);
+    }
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/posts/{post}/comments', [\App\Http\Controllers\CommentController::class, 'index'])->name('comment.index');
+Route::get('/posts/{post}/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'show'])->name('comment.show');
