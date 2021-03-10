1385382148222725a1e80f00a7433a71105a51fa
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-09 21:47:48 -0300

    index() added

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class VolunteerController extends Controller
 {
-    //
+    public function index(){
+        $volunteers = Volunteer::get()->paginate(10);
+
+        return view('volunteers.index', compact('volunteers'));
+    }
 }
