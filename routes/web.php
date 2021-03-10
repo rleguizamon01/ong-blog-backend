2a11eb73d1060f7f53d66a2a421ef138673de651
nicobus <bustamante.n.f@gmail.com>
2021-03-10 18:57:32 -0300

    create suscribers routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('suscribers', 'App\Controller\Http\Controller\SuscriberController');
