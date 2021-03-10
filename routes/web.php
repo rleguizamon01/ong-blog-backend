4857d6e435f19543a2126fc2188be0070618ca43
nicobus <bustamante.n.f@gmail.com>
2021-03-09 23:08:17 -0300

    test routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categories', '\App\Http\Controllers\CategoryController', [
+    'only' => ['index', 'show']
+]);
