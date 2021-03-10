15196920d73f27124a97651897a835cb1417e915
Paz Marzana <pazmarzana@gmail.com>
2021-03-10 18:27:34 -0300

    Updated web routes with categories routes (posts routes already commited)

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('posts', App\Http\Controllers\PostController::class);
+
+Route::resource('categories', App\Http\Controllers\CategoryController::class);
