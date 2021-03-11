e1a340ace7e6f1d1f5eb8179297359981068cb30
nicobus <bustamante.n.f@gmail.com>
2021-03-11 12:34:13 -0300

    fix sintax and grammatical error

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('suscribers', 'App\Controller\Http\Controller\SuscriberController::class');
+Route::resource('subscribers', App\Controller\Http\Controller\SuscriberController::class);
 
