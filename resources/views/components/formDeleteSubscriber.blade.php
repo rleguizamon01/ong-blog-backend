cd5e39907918bfa278dc284dab5443511fe583fc
nicobus <bustamante.n.f@gmail.com>
2021-03-12 22:45:38 -0300

    add form action and delete unnecesary fields

--- resources/views/components/formDeleteSubscriber.blade.php
+++ resources/views/components/formDeleteSubscriber.blade.php
@@ -1,4 +1,12 @@
-<x-formSubscriber>
-    @method('delete')
+<form method="delete" action="{{route('subscribers.destroy', ['hash' => $subscriber->hash])}}">
+    @csrf
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su email" value="{{ old('email') }}">
+        
+        @error('email')
+            <div class="error"> {{ $errors->first('email') }} </div>
+        @enderror
+    </div>
     <button type="submit" class="btn btn-primary">Desuscribirse</button>
-</x-formSubscriber>
+</form>
