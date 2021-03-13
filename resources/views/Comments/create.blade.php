99a71b86bbaab441b96b34a0e25a9c3831aa5bf8
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-12 22:18:12 -0300

    Deleted classes

--- resources/views/Comments/create.blade.php
+++ resources/views/Comments/create.blade.php
@@ -7,19 +7,19 @@
                value="{{ old('email') }}">
 
         @error('email')
-        <p class="error">{{ $errors->first('email') }}</p>
+        <p>{{ $errors->first('email') }}</p>
         @enderror
 
         <br>
 
         <label for="body">Body:</label>
-        <textarea @error('body') class="danger" @enderror name="body" id="body" cols="30" rows="10">
+        <textarea @error('body') @enderror name="body" id="body" cols="30" rows="10">
             {{ old('body') }}
             </textarea>
         @error('body')
-        <p class="error">{{ $errors->first('body') }}</p>
+        <p>{{ $errors->first('body') }}</p>
         @enderror
         <br>
 
-        <button class="button special fit" type="submit">Send</button>
+        <button type="submit">Send</button>
     </form>
