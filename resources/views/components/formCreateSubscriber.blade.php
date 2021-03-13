81a6a98ed57e041ae84d728c56b66d26d070177f
nicobus <bustamante.n.f@gmail.com>
2021-03-12 22:14:39 -0300

    merge archives

--- resources/views/components/formCreateSubscriber.blade.php
+++ resources/views/components/formCreateSubscriber.blade.php
@@ -1,3 +1,30 @@
-<x-formSubscriber>
-    <button type="submit" class="btn btn-primary">Suscribirse</button>
-</x-formSubscriber>
+<form method="post">
+    @csrf
+    <div class="form-group">
+            <label for="first_name">Nombre</label>
+            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Ingrese su nombre ..." value="{{ old('first_name') }}">
+            
+            @error('first_name')
+                <div class="error"> {{ $errors->first('first_name') }} </div>
+            @enderror
+        </div>
+    
+        <div class="form-group">
+            <label for="last_name">Apellido</label>
+            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ingrese su apellido" value="{{ old('last_name') }}">
+    
+            @error('last_name')
+                <div class="error"> {{ $errors->first('last_name') }} </div>
+            @enderror
+        </div>
+    
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su email" value="{{ old('email') }}">
+    
+            @error('email')
+                <div class="error"> {{ $errors->first('email') }} </div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Suscribirse</button>
+</form>
