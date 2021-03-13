17e174712fdb250f997819ba80470b4c34bd6cd1
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-12 22:45:05 -0300

    Fixed view

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -6,26 +6,20 @@
 
     <label for="email">Email: </label>
     <input type="text" name="email"
-
-           @error('email') class="danger" @enderror
            value="{{ $comment->email }}">
 
     @error('email')
-    <p class="error">{{ $errors->first('email') }}</p>
+    <p>{{ $errors->first('email') }}</p>
     @enderror
 
-    <br>
-
     <label for="body">Body:</label>
-    <textarea @error('body') class="danger" @enderror name="body" id="body" cols="30" rows="10">
+    <textarea name="body" id="body" cols="30" rows="10">
          {{ $comment->body }}
      </textarea>
 
     @error('body')
-    <p class="error">{{ $errors->first('body') }}</p>
+    <p>{{ $errors->first('body') }}</p>
     @enderror
 
-    <br>
-
-    <button class="button special fit" type="submit">Send</button>
+    <button type="submit">Send</button>
 </form>
