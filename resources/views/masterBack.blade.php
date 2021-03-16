805e09ccf79dfd6ce0f93386142f5abb3e338a63
Paz Marzana <pazmarzana@gmail.com>
2021-03-16 17:33:36 -0300

    Changed app name

--- resources/views/masterBack.blade.php
+++ resources/views/masterBack.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'ONG Team') }}</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
