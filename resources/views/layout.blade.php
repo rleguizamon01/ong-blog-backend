c0d6c001bd113fe76636c299eb107b9d8b9b3290
Nahuel Savedra <nsavedra97@gmail.com>
2021-03-16 19:38:43 -0300

    Added yield and js files. Fixed code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,23 +3,25 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>ONG</title>
+    <title>ONG Team</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
     <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
     <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/vendor/font-awesome/css/font-awesome.min.css">
     <!-- Custom icon font-->
-    <link rel="stylesheet" href="css/fontastic.css">
+    <link rel="stylesheet" href="/css/fontastic.css">
     <!-- Google fonts - Open Sans-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
     <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <link rel="stylesheet" href="/css/style.default.css" id="theme-stylesheet">
     <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
-
+    <link rel="stylesheet" href="/css/custom.css">
+    <!--Script-->
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 <header class="header">
@@ -28,7 +30,7 @@
         <div class="container">
             <!-- Navbar Brand -->
             <div class="navbar-header d-flex align-items-center justify-content-between">
-                <!-- Navbar Brand --><a href="/" class="navbar-brand">ONG Blog</a>
+                <!-- Navbar Brand --><a href="/" class="navbar-brand">ONG Team</a>
                 <!-- Toggle Button-->
                 <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span></span><span></span><span></span></button>
             </div>
@@ -39,13 +41,14 @@
                     </li>
                     <li class="nav-item"><a href="/posts" class="nav-link ">Blog</a>
                     </li>
-                    <li class="nav-item"><a href="/donations" class="nav-link ">Donations</a>
-                    </li>
                 </ul>
             </div>
         </div>
     </nav>
 </header>
+
+@yield('content')
+
 <!-- Page Footer-->
 <footer class="main-footer">
     <div class="container">
@@ -55,25 +58,15 @@
                     <h6 class="text-white">ONG TEAM</h6>
                 </div>
                 <div class="contact-details">
-                    <p>53 Broadway, Broklyn, NY 11249</p>
-                    <p>Phone: (020) 123 456 789</p>
-                    <p>Email: <a href="mailto:info@company.com">Info@Company.com</a></p>
-                    <ul class="social-menu">
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-behance"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                    </ul>
+                    <p>Alkemy</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="menus d-flex">
                     <ul class="list-unstyled">
-                        <li> <a href="/posts">Blog</a></li>
-                        <li> <a href="/volunteers">Volunteers</a></li>
-                        <li> <a href="/suscribers">Subscribers</a></li>
-                        <li> <a href="/donations">Donations</a></li>
+                        <li> <a href="#">Blog</a></li>
+                        <li> <a href="#">Volunteers</a></li>
+                        <li> <a href="#">Subscribers</a></li>
                     </ul>
                 </div>
             </div>
