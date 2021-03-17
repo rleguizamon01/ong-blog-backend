5dda93eb87035ad1afb97d28605fbcfdb3431079
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-17 07:28:18 -0300

    Subscribers index created

---
+++ resources/views/subscribers/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+
+@section('title', 'Subscribers')
+
+@section('content')
+
+    <div class="container">
+        <div class="row mt-4 row-cols-1 row-cols-md-2">
+            @foreach($subscribers as $subscriber)
+                <div class="col">
+                    <div class="card mb-5 pr-5 pl-5">    
+                        <div class="card-body">
+                            <!-- Names -->
+                            <a href="{{ route('subscribers.show', $subscriber->id) }}" class="text-decoration-none"> 
+                                <h5 class="card-title"> {{ $subscriber->first_name }} {{$subscriber->last_name}} </h5> 
+                            </a>
+                            <h6 class="card-subtitle mb-2 mt-2 text-muted"> {{ $subscriber->email }}</h6>
+                            <div class="d-flex justify-content-between mt-2 mb-2">
+                                <!-- Date -->
+                                <div class="text-uppercase text-muted font-monospace">
+                                    {{ $subscriber->created_at->format('d M Y') }}
+                                </div>
+                            </div>
+                            <p class="card-text"></p>
+                        <!-- Subscriber details -->
+                            <a href="{{ route('subscribers.show', $subscriber->id) }}" class="btn btn-primary">Detalles</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center">
+            {{ $subscribers->links('pagination::bootstrap-4') }}
+    </div>
+
+@endsection
