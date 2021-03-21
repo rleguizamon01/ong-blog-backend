dc983f1bc6a6bcc93b2f599f06440bfd683e83e9
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-21 03:04:47 -0300

    Volunteer controller constructor and policy added

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -7,18 +7,23 @@ use App\Models\Volunteer;
 
 class VolunteerController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Volunteer::class, 'volunteer');
+    }
+
     public function index(){
         return view('volunteers.index', ['volunteers' => Volunteer::paginate(10)]);
     }
 
     public function create()
-    {
+    {    
         //
     }
 
     public function store(Request $request)
     {
-        //
+       //
     }
 
     public function show(Volunteer $volunteer){
