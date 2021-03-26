3273714bb63879ca1c00cb9780a52d2871586879
nicobus <bustamante.n.f@gmail.com>
2021-03-26 11:42:34 -0300

    fix policy

--- app/Policies/SubscriberPolicy.php
+++ app/Policies/SubscriberPolicy.php
@@ -10,10 +10,85 @@ class SubscriberPolicy
 {
     use HandlesAuthorization;
 
-    public function subscriber(Subscriber $subscriber, $hash)
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
     {
-        return $hash === $subscriber->hash;
+        return $user->isAdmin();
     }
 
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return mixed
+     */
+    public function view(User $user, Subscriber $subscriber)
+    {
+        return $user->isAdmin();
+    }
 
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return mixed
+     */
+    public function update(User $user, Subscriber $subscriber)
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return mixed
+     */
+    public function delete(?User $user, Subscriber $subscriber)
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return mixed
+     */
+    public function restore(User $user, Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return mixed
+     */
+    public function forceDelete(User $user, Subscriber $subscriber)
+    {
+        //
+    }
 }
