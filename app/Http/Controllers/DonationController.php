d1fc952655fc0624337626bc1d6adab30f10dffd
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-03-29 11:41:46 -0300

    Donation Controller edited

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -4,82 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Models\Donation;
 use Illuminate\Http\Request;
+use App\Http\Requests\DonationFilterRequest;
 
 class DonationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(DonationFilterRequest $request)
     {
-        //
+        if($request->query('from_date') && $request->query('to_date')){
+            $donations = Donation::whereBetween('created_at', [$request->query('from_date'), $request->query('to_date')])
+                        ->paginate(10)->withQueryString();             
+        }
+        else{
+            $donations = Donation::latest()->paginate(10);
+        }
+
+        return view('admin.donations.index', ['donations' => $donations]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('donations.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Donation  $donation
-     * @return \Illuminate\Http\Response
-     */
     public function show(Donation $donation)
     {
-        //
+        return view('admin.donations.show', ['donation' => $donation]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Donation  $donation
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Donation $donation)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Donation  $donation
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Donation $donation)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Donation  $donation
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Donation $donation)
     {
         //
+    }
+    
+    public function indexAsc(){
+        $donations = Donation::orderBy('amount', 'ASC')->paginate(10);
+
+        return view('admin.donations.index', ['donations' => $donations]);
+    }
+
+    public function indexDesc(){
+        $donations = Donation::orderBy('amount', 'DESC')->paginate(10);
+
+        return view('admin.donations.index', ['donations' => $donations]);
     }
 }
