1a37bd48c3b42de09cfcb42f471abfb24a052d3b
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-04-06 23:03:01 -0300

    Donation filter bug fixed

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -18,7 +18,20 @@ class DonationController extends Controller
 
     public function index(DonationFilterRequest $request)
     {
-        //
+        $query = Donation::query();
+
+        if ($request->filled('from_date') && $request->filled('to_date')) {
+            $query->whereBetween('created_at', [
+                $request->query('from_date'),
+                $request->query('to_date')
+            ]);
+        }
+
+        if($request->filled('order')){
+            $query->orderBy('amount', $request->query('order'));
+        }
+
+        return view('admin.donations.index', ['donations' => $query->paginate(10)->withQueryString()]);
     }
 
     public function create()
