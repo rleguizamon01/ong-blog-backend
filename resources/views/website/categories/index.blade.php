ec4fa785baffa682ee980b83b82724f90831cfe0
Paz Marzana <pazmarzana@gmail.com>
2021-04-07 14:05:26 -0300

    Modified categories view

--- resources/views/website/categories/index.blade.php
+++ resources/views/website/categories/index.blade.php
@@ -1,8 +1,19 @@
-<div class="widget categories">
+<div id="categories" class="widget categories">
     <header>
         <h3 class="h6">Categorias</h3>
     </header>
     @foreach($categories as $category)
-        <div class="item d-flex justify-content-between"><a href="{{ route('categories.posts.index', $category->id) }}">{{$category->name}}</a><span>{{$category->posts_count}}</span></div>
+        <div class="item d-flex justify-content-between">
+            <a href="{{ route('categories.posts.index', $category->id) }}">
+                {{$category->name}}
+            </a>
+            <span>{{$category->posts_count}}</span>
+        </div>
     @endforeach
+        <div class="item d-flex justify-content-between">
+            <a href="{{ route('categories.posts.index', "") }}">
+                Todas
+            </a>
+            <span>{{$totalposts}}</span>
+        </div>
 </div>
