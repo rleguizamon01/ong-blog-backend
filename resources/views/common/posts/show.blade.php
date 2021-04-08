9b45d2bd08c54fcac60ae45fd569e603993cea45
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-04-08 05:29:12 -0300

    Post show view changed

--- resources/views/common/posts/show.blade.php
+++ resources/views/common/posts/show.blade.php
@@ -10,44 +10,67 @@
                         <div class="title">Categoria: {{$post->category->name}}</div>
                     </div>
                 </div>
-                <!--Usuario-->
-                <div class="post-footer d-flex align-items-center flex-column flex-sm-row"><div class="author d-flex align-items-center flex-wrap">
-                        <div class="title"><i class="fas fa-user"></i><span>{{ $post->user->first_name }} {{ $post->user->last_name }}</span></div></div>
+
+                <!--User-->
+                <div class="post-footer d-flex flex-column flex-sm-row">
+                    <div class="author d-flex align-items-center flex-wrap">
+                        <div class="title">
+                            <i class="fas fa-user"></i>
+                            <span>{{ $post->user->first_name }} {{ $post->user->last_name }}</span>
+                        </div>
+                    </div>
                     <div class="d-flex align-items-center flex-wrap">
-                        <div class="date"><i class="fas fa-table"></i>{{ $post->created_at->format('d M Y') }}</div>
-                        <div class="comments meta-last"><i class="fas fa-comments"></i>{{ count($post->comments) }}</div>
+                        <div class="date">
+                            <i class="fas fa-table"></i>
+                            {{ $post->created_at->format('d M Y') }}
+                        </div>
+                        <div class="comments meta-last">
+                            <i class="fas fa-comments"></i>
+                            {{ count($post->comments) }}
+                        </div>
                     </div>
                 </div>
-                    <h1>{{$post->title}}</h1>
 
-                    <div class="post-body">
-                        <p>{{$post->body}}</p>
-                    </div>
+                <h1>{{$post->title}}</h1>
 
-                    <div class="post-comments">
-                        <header>
-                            <h3 class="h6">Comentarios del post<span class="no-of-comments">{{ count($post->comments) }}</span></h3>
-                        </header>
-                        <!--comments-->
-                        @foreach($post->comments as $comment)
-                        <div class="comment">
-                            <div class="comment-header d-flex justify-content-between">
-                                <div class="user d-flex align-items-center">
-                                    <div class="title"><strong>{{$comment->email}}</strong></div>
-                                </div>
-                            </div>
-                            <div class="comment-body">
-                                <p>{{$comment->body}}</p>
-                            </div>
-                        </div>
-                        @endforeach
+                <div class="post-body">
+                    <p>{{$post->body}}</p>
+                </div>
+
+                <div class="post-comments">
+                    <header>
+                        <h3 class="h6">Comentarios del post<span class="no-of-comments">{{ count($post->comments) }}</span></h3>
+                    </header>
 
                     <div class="add-comment">
-                        @include('website.comments.create', ['post' => $post])
+                        <header>
+                            <h3 class="h6">Deja tu comentario</h3>
+                        </header>
+                        <div class="row mb-3">
+                            <div class="form-group col-md-12">
+                                <input type="email" name="email" id="email" placeholder="Direccion de email" class="form-control">
+                                
+                                <!-- Email error -->
+                                <p id="email-error" class="d-none alert alert-danger mt-3 small p-2">{{ $errors->first('email') }}</p>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <textarea name="body" id="body" placeholder="Tu comentario" class="form-control"></textarea>
+
+                                <!-- Body error -->
+                                <p id="body-error" class=" d-none alert alert-danger mt-3 small p-2"></p>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <button id="submit-comment" class="btn btn-secondary">Enviar comentario</button>
+                            </div>
+                        </div>
                     </div>
 
-                </div>
+                    <!-- Comments -->
+                    <div id="comment-section"></div> 
+
+                </div>                    
             </div>
         </div>
     </main>
 @endsection
+
