62132664d4d7a3ed8144af1ce9cfa216a100f384
nicobus <bustamante.n.f@gmail.com>
2021-04-12 18:02:48 -0300

    change filter route

--- resources/views/admin/subscribers/index.blade.php
+++ resources/views/admin/subscribers/index.blade.php
@@ -19,7 +19,7 @@
         <h2>
             Listado de Suscriptores
         </h2>
-        <form method='GET' action="{{route('admin.subscribers.filter')}}">
+        <form method='GET' action="{{route('admin.subscribers.index')}}">
             <div class="form-row align-items-end">
                 <div class="form-group col-md-2">
                     <label for="status">Estado:</label>
