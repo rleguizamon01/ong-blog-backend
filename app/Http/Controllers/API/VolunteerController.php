f81c7195253dca9d79f34212da0b71cd4a74ae61
nicobus <bustamante.n.f@gmail.com>
2021-05-03 17:45:35 -0300

    volunteers API

--- app/Http/Controllers/API/VolunteerController.php
+++ app/Http/Controllers/API/VolunteerController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Volunteer;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VolunteerConfirmation;
 
 class VolunteerController extends Controller
 {
@@ -13,5 +15,20 @@ class VolunteerController extends Controller
         $volunteers = Volunteer::byStatus($request->get('status'))->paginate(10)->toJson();
 
         return response($volunteers,200);
+    }
+
+    public function store(Request $request)
+    {
+        $volunteer = Volunteer::create($request->all());
+
+        Mail::send(new VolunteerConfirmation($volunteer));
+
+        return response()->json($volunteer, 201);
+    }
+
+    public function destroy(Volunteer $volunteer)
+    {
+         $volunteer->delete();
+         return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('volunteers', App\Http\Controllers\API\VolunteerController::class)->only(['index','show']);
+Route::apiResource('volunteers', App\Http\Controllers\API\VolunteerController::class);
